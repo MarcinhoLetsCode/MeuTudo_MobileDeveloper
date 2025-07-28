33abe025e09172d9f25f1bd9b67d89cdf02c579c
Márcio Darlan <darlan_lifecat@yahoo.com.br>
2025-07-28 17:22:09 -0300

    Iniciando Projeto Simulador Mario Kart

---
+++ node-dio/03 - Simulador de Corridas do Mario Kart Com Node/src/index.js
@@ -0,0 +1,54 @@
+const player1 = {
+    NOME : "Mario",
+    VELOCIDADE : 4,
+    MANOBRABILIDADE : 3,
+    PODER : 3,
+    PONTOS : 0,
+};
+
+const player2 = {
+    NOME : "Peach",
+    VELOCIDADE : 3,
+    MANOBRABILIDADE : 4,
+    PODER : 2,
+    PONTOS : 0,
+};
+
+const player3 = {
+    NOME : "Yoshi",
+    VELOCIDADE : 2,
+    MANOBRABILIDADE : 4,
+    PODER : 3,
+    PONTOS : 0,
+};
+
+const player4 = {
+    NOME : "Bowser",
+    VELOCIDADE : 5,
+    MANOBRABILIDADE : 2,
+    PODER : 5,
+    PONTOS : 0,
+};
+
+const player5 = {
+    NOME : "Luigi",
+    VELOCIDADE : 3,
+    MANOBRABILIDADE : 4,
+    PODER : 4,
+    PONTOS : 0,
+};
+
+const player6 = {
+    NOME : "DOnkey Kong",
+    VELOCIDADE : 2,
+    MANOBRABILIDADE : 2,
+    PODER : 5,
+    PONTOS : 0,
+};
+
+async function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+    //console.log(Math.floor(Math.random() * 6) + 1);
+}
+
+//rollDice();
