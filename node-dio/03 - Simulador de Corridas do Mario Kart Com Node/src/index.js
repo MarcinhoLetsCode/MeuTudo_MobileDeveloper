d8b200e759022915cc273543434521ad3885965d
Márcio Darlan <darlan_lifecat@yahoo.com.br>
2025-08-05 17:04:32 -0300

    Implementando confrontos e condicoes de vitoria

--- node-dio/03 - Simulador de Corridas do Mario Kart Com Node/src/index.js
+++ node-dio/03 - Simulador de Corridas do Mario Kart Com Node/src/index.js
@@ -70,8 +70,10 @@ async function noRepeat() {
     if (racers[p1].NOME!==racers[cpu-1].NOME){
         //console.log(cpu);
         //console.log(player1.NOME);
-        c1 = racers[cpu-1].NOME;
-        console.log(`Oponente ${c1}`);
+        //c1 = racers[cpu-1].NOME;
+        c1 = cpu-1;
+        //console.log(`Oponente ${c1}`);
+        console.log(`Oponente ${racers[c1].NOME}`);
     }
         
 }
@@ -177,13 +179,160 @@ async function validPlayer() {
     });
 }
 
-//AUTO INVOKE, NA PRECISA 'MAIN();'
+async function getRandomBlock(){
+    let random = Math.random();
+    let result;
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA"
+            break;
+        case random < 0.66:
+            result = "CURVA";
+            break;    
+        default:
+            result = "CONFRONTO";
+            break;
+    }
+
+    return result;
+}
+
+async function logRollResult(characterName, block, diceResult, attribute){
+    console.log(`${characterName}🎲 rolou um dado de ${block} ${diceResult} + ${attribute} = ${diceResult + attribute}`);
+}
+
+async function playRaceEngine(character1, character2){
+    for (let round = 1; round <= 5; round++) {
+        console.log(`🏁 Rodada ${round}`);
+
+        //Sortear Bloco
+        let block = await getRandomBlock();
+        console.log(`Bloco: ${block}`)
+        
+        //ROLLING DICES
+        let diceResult1 = await rollDice();
+        let diceResult2 = await rollDice();
+
+        //HABILITY TEST
+        let totalTestSkill1 = 0;
+        let totalTestSkill2 = 0;
+
+        if (block === "RETA") {
+            totalTestSkill1 = diceResult1 + character1.VELOCIDADE;
+            totalTestSkill2 = diceResult2 + character2.VELOCIDADE;
+
+            //console.log(`${player1.NOME}🎲 rolou um dado de ${block} ${diceResult1}`);
+            //console.log(`${player2.NOME}🎲 rolou um dado de ${block} ${diceResult2}`);
+
+            await logRollResult(
+                player1.NOME, 
+                "Velocidade", 
+                diceResult1, 
+                character1.VELOCIDADE
+            );
+
+            await logRollResult(
+                player2.NOME, 
+                "Velocidade", 
+                diceResult2, 
+                character2.VELOCIDADE
+            );
+
+        } else if (block === "CURVA") {
+            totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
+            totalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE;
+
+            await logRollResult(
+                player1.NOME, 
+                "Manobrabilidade", 
+                diceResult1, 
+                character1.MANOBRABILIDADE
+            );
+
+            await logRollResult(
+                player2.NOME, 
+                "Manobrabilidade", 
+                diceResult2, 
+                character2.MANOBRABILIDADE
+            );
+
+        } else if (block === "CONFRONTO") {
+            let powerResult1 = diceResult1 + character1.PODER;
+            let powerResult2 = diceResult2 + character2.PODER;
+
+            console.log(`${character1.NOME}🥊 confrontou com ${character2.NOME}🥊!`);
+
+            await logRollResult(
+                player1.NOME, 
+                "Poder", 
+                diceResult1, 
+                character1.PODER
+            );
+
+            await logRollResult(
+                player2.NOME, 
+                "Poder", 
+                diceResult2, 
+                character2.PODER
+            );
+
+            character2.PONTOS -= powerResult1 > powerResult2 && character2.PONTOS > 0 ? 1 + console.log(`${character1.NOME} ganhou!\n 
+                ${character2.NOME} perde ponto`) : 0;
+            character1.PONTOS -= powerResult1 < powerResult2 && character1.PONTOS > 0 ? 1 + console.log(`${character2.NOME} ganhou!\n 
+                ${character1.NOME} perde ponto`) : 0;
+            console.log(
+                powerResult1 === powerResult2
+                ? "Confronto empatado, ninguém perde ponto!"
+                : ""
+            );
+
+            /* if (powerResult1 > powerResult2) {
+                console.log(`${character1.NOME} ganhou!`);
+                if (character2.PONTOS > 0) {
+                    console.log(`${character2.NOME} perde ponto`);
+                    character2.PONTOS--;
+
+                }
+            } else if (powerResult1 < powerResult2) {
+                console.log(`${character2.NOME} ganhou!`);
+                if (character2.PONTOS > 0) {
+                    console.log(`${character2.NOME} perde ponto`);
+                    character1.PONTOS--;
+                }
+            } else if (powerResult1 === powerResult2) {
+                console.log(`Confronto empatado, ninguém perde ponto!`);
+            } */
+        }
+
+        // VERIFICANDO O VENCEDOR
+        if (totalTestSkill1 > totalTestSkill2) {
+            console.log(`${character1.NOME} marcou um ponto!`);
+            character1.PONTOS++;
+        } else if (totalTestSkill1 < totalTestSkill2) {
+            console.log(`${character2.NOME} marcou um ponto!`);
+            character2.PONTOS++;
+        } else if (totalTestSkill1 != 0 && totalTestSkill2 != 0) {
+            console.log(`${character1.NOME} e ${character2.NOME} empataram!`);
+            console.log("Ninguém marcou!");
+        }
+
+        console.log("----------------------------------------------------------------------------------------");
+    }
+}
+
+//AUTO INVOKE, SEM CHAMADA 'MAIN();'
 (async function main(){
-    await selectPlayer();    
-    console.log(`\nSelecionou ${racers[(p1)].NOME}`);
-    c1 = cpuPlayer();
-    setTimeout(function() {
-        console.log(`\n🏁 🚦 Corrida Entre ${racers[(p1)].NOME} e ${c1} Começando...\n`);
-    }, 2000);
+    //await selectPlayer();    
+    //console.log(`\nSelecionou ${racers[(p1)].NOME}`);
+    //c1 = cpuPlayer();
+    //setTimeout(function() {
+        //EMOJI 'Windows + .'
+        //console.log(`\n🏁 🚦 Corrida Entre ${racers[(p1)].NOME} e ${racers[(c1)].NOME} Começando...\n`);
+    //}, 2000);
+
+    console.log(`\n🏁 🚦 Corrida Entre ${player1.NOME} e ${player2.NOME} Começando...\n`);
+
+    await playRaceEngine(player1, player2);
     
 })();
